ea5b0cbc0d69479b1ae6f9e5bfadc32b9c74a184
minatisleeping <minatt2002@gmail.com>
2024-08-16 12:51:17 +0700

    Nested route to use outlet..

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,9 +12,6 @@ const Header = () => {
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            {/* <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/user'>User</Nav.Link>
-            <Nav.Link href='/admin'>Admin</Nav.Link> */}
             <Link to='/' className='nav-link'>Home</Link>
             <Link to='/user' className='nav-link'>User</Link>
             <Link to='/admin' className='nav-link'>Admin</Link>
