ff6dc2a803347d5e0d83b034b6495c7d40b2427b
minatisleeping <minatt2002@gmail.com>
2024-08-16 12:26:33 +0700

    Redirect w link(react-router-dom)..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.scss'
 import Header from './components/Header/Header'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        test link
+        <div>
+          <button><Link to="/user">Go to user page</Link></button>
+          <button><Link to="/admin">Go to admin page</Link></button>
+        </div>
+      </div>
     </div>
   )
 }
