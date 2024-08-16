797fd7a1a323ce7bf334d997d66fda454c282138
minatisleeping <minatt2002@gmail.com>
2024-08-16 11:36:13 +0700

    code header..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss'
+import Header from './components/Header/Header'
 
 const App = () => {
   return (
-    <div className="App">
-      heloooo
-      
+    <div className="app-container">
+      <Header />
     </div>
   )
 }
