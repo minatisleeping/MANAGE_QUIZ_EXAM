983ab73a058237e88af10932b0982312dffadc87
minatisleeping <minatt2002@gmail.com>
2024-08-19 10:35:08 +0700

    Desing home header..

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
@@ -18,11 +18,13 @@ const Header = () => {
             <NavLink to='/admin' className='nav-link'>Admin</NavLink>
           </Nav>
           <Nav>
-          <NavDropdown title='Settings' id='basic-nav-dropdown'>
+            <button className='btn-login'>Log In</button>
+            <button className='btn-signup'>Sign Up</button>
+            {/* <NavDropdown title='Settings' id='basic-nav-dropdown'>
               <NavDropdown.Item>Login</NavDropdown.Item>
               <NavDropdown.Item>Logout</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
