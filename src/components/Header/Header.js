e621540ba5805940da845f0895f64f6f9436062e
minatisleeping <minatt2002@gmail.com>
2024-08-30 11:03:20 +0700

    use redux to store user info..

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
+import { useSelector } from 'react-redux'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated)
+  // const account = useSelector((state) => state.user.account)
+
   const navigate = useNavigate()
 
   const handleLogin = () => navigate('/login')
@@ -14,22 +18,38 @@ const Header = () => {
     <Navbar expand='lg' bg='light'>
       <Container>
         {/* <Navbar.Brand href='#home'>Minat</Navbar.Brand> */}
-        <NavLink to='/' className='navbar-brand'>Minat</NavLink>
+        <NavLink to='/' className='navbar-brand'>
+          Minat
+        </NavLink>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <NavLink to='/' className='nav-link'>Home</NavLink>
-            <NavLink to='/user' className='nav-link'>User</NavLink>
-            <NavLink to='/admin' className='nav-link'>Admin</NavLink>
+            <NavLink to='/' className='nav-link'>
+              Home
+            </NavLink>
+            <NavLink to='/user' className='nav-link'>
+              User
+            </NavLink>
+            <NavLink to='/admin' className='nav-link'>
+              Admin
+            </NavLink>
           </Nav>
           <Nav>
-            <button className='btn-login' onClick={() => handleLogin()}>Log In</button>
-            <button className='btn-signup' onClick={() => handleRegister()}>Sign Up</button>
-            {/* <NavDropdown title='Settings' id='basic-nav-dropdown'>
-              <NavDropdown.Item>Login</NavDropdown.Item>
-              <NavDropdown.Item>Logout</NavDropdown.Item>
-              <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown> */}
+            {isAuthenticated === false ? (
+              <>
+                <button className='btn-login' onClick={() => handleLogin()}>
+                  Log In
+                </button>
+                <button className='btn-signup' onClick={() => handleRegister()}>
+                  Sign Up
+                </button>
+              </>
+            ) : (
+              <NavDropdown title='Settings' id='basic-nav-dropdown'>
+                <NavDropdown.Item>Logout</NavDropdown.Item>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/redux/action/counterAction.js
+++ src/redux/action/counterAction.js
@@ -1,15 +1,10 @@
 export const INCREMENT = 'INCREMENT'
-
 export const DECREMENT = 'DECREMENT'
 
 export const increaseCounter = () => {
-    return {
-        type: INCREMENT,
-    }
+  return { type: INCREMENT }
 }
 
 export const decreaseCounter = () => {
-    return {
-        type: DECREMENT,
-    }
+  return { type: DECREMENT }
 }
