f408cb475b2076cb1fe7039a80dc908c8db88ab3
minatisleeping <minatt2002@gmail.com>
2024-08-31 12:39:50 +0700

    handle data..

--- src/components/User/DetailedQuiz.js
+++ src/components/User/DetailedQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getQuizById } from '../../services/apiService'
+import _ from 'lodash'
 
 const DetailedQuiz = () => {
   const params = useParams()
@@ -13,6 +14,26 @@ const DetailedQuiz = () => {
   const fetchQuestions = async () => {
     const res = await getQuizById(quizId)
     console.log('🚀 ~ res:', res)
+    if (res && res.EC === 0) {
+      let raw = res.DT
+      let data = _.chain(raw)
+        .groupBy("id")
+        .map((value, key) => {
+          let answers = []
+          let questionDesc, image = null
+          value.forEach((item, index) => {
+            if (index === 0) { // vòng lặp đầu tiên lấy ra questionDesc và image vì nó không thay đổi
+              questionDesc = item.description
+              image = item.image
+            }
+            answers.push(item.answers)
+          })
+          
+          return { questionId: key, questionDesc, answers, image }
+        })
+        .value()
+      console.log('🚀 ~ data:', data)
+    }
   }
 
   return(
