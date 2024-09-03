c1e585129d0e94bdc502e0a55b8d88a4d907693e
minatisleeping <minatt2002@gmail.com>
2024-09-03 10:21:51 +0700

    build UI show question..

---
+++ src/components/User/Question.js
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+
+const Question = (props) => {
+  const { data, index } = props  
+
+  if (_.isEmpty(data)) return (<></>)
+
+  return (
+    <>
+      {data.image &&
+        <div className='q-image'>
+          <img src={`data:image/png;base64, ${data.image}` } alt='img-question'/>
+        </div>
+      }
+      <div className='question'>Question {index + 1}: {data.questionDesc}?</div>
+      <div className='answer'>
+        {data.answers && data.answers.length && data.answers.map((item, index) => {
+          return (
+            <div key={`answer-${index}`}>
+              <div class='form-check'>
+                <input class='form-check-input' type='checkbox' value=''/>
+                <label class='form-check-label'>
+                  {item.description}
+                </label>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+ 
+export default Question
