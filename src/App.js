599aac56564e58e2172cee03cdd282c799951ab8
minatisleeping <minatt2002@gmail.com>
2024-09-13 11:33:04 +0700

    css..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss'
 import Header from './components/Header/Header'
 import { Outlet } from 'react-router-dom'
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 const App = () => {
   return (
@@ -9,11 +10,12 @@ const App = () => {
         <Header />
       </div>
       <div className='main-container'>
-        
         <div className='sidebar-container'>
         </div>
         <div className='app-content'>
-          <Outlet />
+          <PerfectScrollbar>
+            <Outlet />
+          </PerfectScrollbar>
         </div>
       </div>
     </div>
